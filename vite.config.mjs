296d3b1d8dddea293fa6142fdff7844e58c007ba
MNO-proA <michaelnanaofosuofficial@gmail.com>
2024-07-07 12:48:38 +0000

    update

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,10 +1,11 @@
+// vite.config.js or vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: "build",
+    outDir: "dist",  // Update this line to 'dist'
   },
   plugins: [react()],
 });
